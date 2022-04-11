18739c0a0faeabb64381076019ef24f8b1cca05f
Muhammed <muhammed.almazbekovich@gmail.com>
2022-04-11 06:22:28 +0300

    Готов

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -2,11 +2,6 @@ package com.company;
 
 public class Car  implements AutoCloseable{
 
-    private final int mashinadanTushuu = 1;
-    private final int mashinagaOturuu = 1;
-    boolean mashinadanTushuu1 = true;
-    boolean mashinagaOturuu1 = true;
-
     public void close(){
         System.out.println("Машина жабылып жатат");
     }
@@ -17,9 +12,13 @@ public class Car  implements AutoCloseable{
             int mashinaJurupJatkandaMashinadanTushuu = 0;
             int mashinaJurupJatkandaMashinagaOturuu = 0;
 
+            boolean mashinadanTushuu1 = true;
+            boolean mashinagaOturuu1 = true;
             if (mashinadanTushuu1) {
+                int mashinadanTushuu = 1;
                 System.out.println(mashinadanTushuu / mashinaJurupJatkandaMashinadanTushuu);
             } else if (mashinagaOturuu1) {
+                int mashinagaOturuu = 1;
                 System.out.println(mashinagaOturuu / mashinaJurupJatkandaMashinagaOturuu);
             }
         } catch(RuntimeException exception){
